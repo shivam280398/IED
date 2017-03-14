944b53d1ab583cddb9b1f88858bdc8e932bd15eb
shivam280398 <shivam16195@iiitd.ac.in>
2017-03-14 17:21:20 +0530

    init

---
+++ app/src/main/java/com/example/shivam/ied/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.example.shivam.ied;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
